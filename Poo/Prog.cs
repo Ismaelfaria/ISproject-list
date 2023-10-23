714383ce46b8be9f2a4b0b127fc5af1e6bbbabca
limaismael8901 <limaismael8901@gmail.com>
2023-10-23 16:59:45 -0300

    Adicionar arquivos de projeto.

---
+++ Poo/Prog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo
+{
+    internal class Prog
+    {
+        static void Main(string[] args)
+        {
+            var usuario = new Usuario();
+            usuario.Nome = "Davi";
+            usuario.Buscar();
+            Console.WriteLine(usuario.Nome); 
+        }
+    }
+}
