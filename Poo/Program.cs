6f3c21e4f261bc37d7c99c1483a81b66452bb733
limaismael8901 <limaismael8901@gmail.com>
2023-11-05 20:13:01 -0300

    att

---
+++ Poo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var v = new LoginVerification("Alex", "12345");
+        }
+    }
+}
---
+++ Poo/UserDatabase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    internal class UserDatabase : UserModel
+    {
+        public static List<UserModel> _users = new()
+        {
+            new UserModel{ UserName = "Alex", UserPassword = "12345"},
+            new UserModel{ UserName = "Davi", UserPassword = "12345"},
+            new UserModel{ UserName = "Daniel", UserPassword = "12345"},
+            new UserModel{ UserName = "Roberto", UserPassword = "12345"}
+        };
+    }
+}
